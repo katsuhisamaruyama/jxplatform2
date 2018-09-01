08891f380649df83ccfb58b4c270cf4a5c902441
Katsuhisa Maruyama <maru@cs.ritsumei.ac.jp>
2018-09-01 14:44:36 +0900

    For plug-in use

---
+++ org.jtool.eclipse.test.plugin/src/org/jtool/eclipse/plugin/Activator.java
@@ -0,0 +1,41 @@
+/*
+ *  Copyright 2018
+ *  Software Science and Technology Lab.
+ *  Department of Computer Science, Ritsumeikan University
+ */
+
+package org.jtool.eclipse.plugin;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator that controls the plug-in life cycle.
+ * 
+ * @author Katsuhisa Maruyama
+ */
+public class Activator extends AbstractUIPlugin {
+    
+    public static final String PLUGIN_ID = "org.eclipse.test.plugin";
+    
+    private static Activator plugin;
+    
+    public Activator() {
+    }
+    
+    @Override
+    public void start(BundleContext context) throws Exception {
+        super.start(context);
+        plugin = this;
+    }
+    
+    @Override
+    public void stop(BundleContext context) throws Exception {
+        plugin = null;
+        super.stop(context);
+    }
+    
+    public static Activator getDefault() {
+        return plugin;
+    }
+}
