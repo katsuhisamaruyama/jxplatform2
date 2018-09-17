649397c6d0134ecd59d319a277f3dd1c5379bf7f
Katsuhisa Maruyama <maru@cs.ritsumei.ac.jp>
2018-09-17 16:31:36 +0900

    Refactoring

---
+++ org.jtool.eclipse/src/main/java/org/jtool/eclipse/javamodel/builder/BytecodeClassInfo.java
@@ -0,0 +1,99 @@
+/*
+ *  Copyright 2018
+ *  Software Science and Technology Lab.
+ *  Department of Computer Science, Ritsumeikan University
+ */
+
+package org.jtool.eclipse.javamodel.builder;
+
+import org.jtool.eclipse.javamodel.JavaClass;
+import javassist.CtClass;
+import javassist.NotFoundException;
+import java.util.Set;
+import java.util.HashSet;
+
+/**
+ * An object that represents a class restored from its bytecode.
+ * This class uses Javassit modules.
+ * 
+ * All methods of this class are not intended to be directly called by clients.
+ * 
+ * @author Katsuhisa Maruyama
+ */
+
+class BytecodeClassInfo {
+    
+    private CtClass ctClass;
+    private Set<BytecodeClassInfo> parents = new HashSet<BytecodeClassInfo>();
+    private Set<BytecodeClassInfo> children = new HashSet<BytecodeClassInfo>();
+    
+    private Set<CtClass> ancestors = new HashSet<CtClass>();
+    private Set<CtClass> descendants = new HashSet<CtClass>();
+    
+    private Set<JavaClass> jdescendants = new HashSet<JavaClass>();
+    
+    BytecodeClassInfo(CtClass ctClass) {
+        this.ctClass = ctClass;
+        
+        try {
+            CtClass superClass = ctClass.getSuperclass();
+            if (superClass != null) {
+                parents.add(new BytecodeClassInfo(superClass));
+            }
+            for (CtClass c : ctClass.getInterfaces()) {
+                parents.add(new BytecodeClassInfo(c));
+            }
+        } catch (NotFoundException e) { /* empty */ }
+    }
+    
+    String getName() {
+        return ctClass.getName();
+    }
+    
+    CtClass getCtClass() {
+        return ctClass;
+    }
+    
+    Set<BytecodeClassInfo> getParents() {
+        return parents;
+    }
+    
+    Set<BytecodeClassInfo> getChildren() {
+        return children;
+    }
+    
+    void addChild(BytecodeClassInfo child) {
+        children.add(child);
+    }
+    
+    Set<CtClass> getAncestors() {
+        return ancestors;
+    }
+    
+    void setAncestors(BytecodeClassInfo classInfo) {
+        for (BytecodeClassInfo parent : classInfo.getParents()) {
+            ancestors.add(parent.getCtClass());
+            setAncestors(parent);
+        }
+    }
+    
+    Set<CtClass> getDescendants() {
+        return descendants;
+    }
+    
+    void setDescendants(BytecodeClassInfo classInfo) {
+        for (BytecodeClassInfo child : classInfo.getChildren()) {
+            descendants.add(child.getCtClass());
+            setDescendants(child);
+        }
+    }
+    
+    
+    void addJavaDescendant(JavaClass jdescendant) {
+        jdescendants.add(jdescendant);
+    }
+    
+    Set<JavaClass> getJavaDescendants() {
+        return jdescendants;
+    }
+}
--- org.jtool.eclipse/src/main/java/org/jtool/eclipse/javamodel/builder/BytecodeClassStore.java
+++ org.jtool.eclipse/src/main/java/org/jtool/eclipse/javamodel/builder/BytecodeClassStore.java
@@ -25,7 +25,7 @@ import java.util.zip.ZipFile;
 import java.util.zip.ZipEntry;
 
 /**
- * An object holds a collection of classes restored from its bytecode.
+ * An object that stores classes restored from its bytecode.
  * This class uses Javassit modules.
  * 
  * All methods of this class are not intended to be directly called by clients.
@@ -250,83 +250,6 @@ public class BytecodeClassStore {
                     }
                 }
             }
-        }
-    }
-    
-    private class BytecodeClassInfo {
-        
-        private CtClass ctClass;
-        private Set<BytecodeClassInfo> parents = new HashSet<BytecodeClassInfo>();
-        private Set<BytecodeClassInfo> children = new HashSet<BytecodeClassInfo>();
-        
-        private Set<CtClass> ancestors = new HashSet<CtClass>();
-        private Set<CtClass> descendants = new HashSet<CtClass>();
-        
-        private Set<JavaClass> jdescendants = new HashSet<JavaClass>();
-        
-        BytecodeClassInfo(CtClass ctClass) {
-            this.ctClass = ctClass;
-            
-            try {
-                CtClass superClass = ctClass.getSuperclass();
-                if (superClass != null) {
-                    parents.add(new BytecodeClassInfo(superClass));
-                }
-                for (CtClass c : ctClass.getInterfaces()) {
-                    parents.add(new BytecodeClassInfo(c));
-                }
-            } catch (NotFoundException e) { /* empty */ }
-        }
-        
-        String getName() {
-            return ctClass.getName();
-        }
-        
-        CtClass getCtClass() {
-            return ctClass;
-        }
-        
-        Set<BytecodeClassInfo> getParents() {
-            return parents;
-        }
-        
-        Set<BytecodeClassInfo> getChildren() {
-            return children;
-        }
-        
-        void addChild(BytecodeClassInfo child) {
-            children.add(child);
-        }
-        
-        Set<CtClass> getAncestors() {
-            return ancestors;
-        }
-        
-        void setAncestors(BytecodeClassInfo classInfo) {
-            for (BytecodeClassInfo parent : classInfo.getParents()) {
-                ancestors.add(parent.getCtClass());
-                setAncestors(parent);
-            }
-        }
-        
-        Set<CtClass> getDescendants() {
-            return descendants;
-        }
-        
-        void setDescendants(BytecodeClassInfo classInfo) {
-            for (BytecodeClassInfo child : classInfo.getChildren()) {
-                descendants.add(child.getCtClass());
-                setDescendants(child);
-            }
-        }
-        
-        
-        void addJavaDescendant(JavaClass jdescendant) {
-            jdescendants.add(jdescendant);
-        }
-        
-        Set<JavaClass> getJavaDescendants() {
-            return jdescendants;
         }
     }
 }
