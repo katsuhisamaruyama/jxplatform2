e7ed50e3426c7893c5aecb2223f71db9f9f2512e
Katsuhisa Maruyama <maru@cs.ritsumei.ac.jp>
2018-09-18 23:05:37 +0900

    Refactoring

--- org.jtool.eclipse/src/main/java/org/jtool/eclipse/javamodel/builder/ModelBuilder.java
+++ org.jtool.eclipse/src/main/java/org/jtool/eclipse/javamodel/builder/ModelBuilder.java
@@ -6,9 +6,17 @@
 
 package org.jtool.eclipse.javamodel.builder;
 
+import org.jtool.eclipse.cfg.CCFG;
+import org.jtool.eclipse.cfg.CFG;
 import org.jtool.eclipse.cfg.builder.CFGStore;
+import org.jtool.eclipse.pdg.ClDG;
+import org.jtool.eclipse.pdg.PDG;
+import org.jtool.eclipse.pdg.SDG;
 import org.jtool.eclipse.pdg.builder.PDGStore;
 import org.jtool.eclipse.cfg.builder.JInfoStore;
+import org.jtool.eclipse.javamodel.JavaClass;
+import org.jtool.eclipse.javamodel.JavaField;
+import org.jtool.eclipse.javamodel.JavaMethod;
 import org.jtool.eclipse.javamodel.JavaProject;
 import org.jtool.eclipse.util.Logger;
 
@@ -61,6 +69,86 @@ public abstract class ModelBuilder {
             ProjectStore.getInstance().removeProject(currentProject.getPath());
             currentProject.clear();
         }
+    }
+    
+    public CFG getCFG(String fqn) {
+        return CFGStore.getInstance().getCFG(fqn);
+    }
+    
+    public int sizeOfCFG() {
+        return CFGStore.getInstance().size();
+    }
+    
+    public CCFG getCCFG(JavaClass jclass) {
+        return CFGStore.getInstance().getCCFG(jclass);
+    }
+    
+    public CFG getCFG(JavaMethod jmethod) {
+        return CFGStore.getInstance().getCFG(jmethod);
+    }
+    
+    public CFG getCFG(JavaField jfield) {
+        return CFGStore.getInstance().getCFG(jfield);
+    }
+    
+    public int sizeOfPDG() {
+        return PDGStore.getInstance().size();
+    }
+    
+    public PDG getPDG(String fqn) {
+        return PDGStore.getInstance().getPDG(fqn);
+    }
+    
+    public PDG getPDG(CFG cfg) {
+        return PDGStore.getInstance().getPDG(cfg);
+    }
+    
+    public PDG getPDG(JavaMethod jmethod) {
+        return PDGStore.getInstance().getPDG(jmethod);
+    }
+    
+    public PDG getPDG(JavaField jfield) {
+        return PDGStore.getInstance().getPDG(jfield);
+    }
+    
+    public PDG getPDGWithinSDG(JavaMethod jmethod) {
+        return PDGStore.getInstance().getPDGWithinSDG(jmethod);
+    }
+    
+    public PDG getPDGWithinSDG(JavaField jfield) {
+        return PDGStore.getInstance().getPDGWithinSDG(jfield);
+    }
+    
+    public ClDG getClDG(JavaClass jclass) {
+        return PDGStore.getInstance().getClDG(jclass);
+    }
+    
+    public ClDG getClDGWithinSDG(JavaClass jclass) {
+        return PDGStore.getInstance().getClDGWithinSDG(jclass);
+    }
+    
+    public ClDG getClDG(JavaMethod jmethod) {
+        return PDGStore.getInstance().getClDG(jmethod);
+    }
+    
+    public ClDG getClDGWithinSDG(JavaMethod jmethod) {
+        return PDGStore.getInstance().getClDGWithinSDG(jmethod);
+    }
+    
+    public ClDG getClDG(JavaField jfield) {
+        return PDGStore.getInstance().getClDG(jfield);
+    }
+    
+    public ClDG getClDGWithinSDG(JavaField jfield) {
+        return PDGStore.getInstance().getClDGWithinSDG(jfield);
+    }
+    
+    public SDG getSDG(JavaClass jclass) {
+        return PDGStore.getInstance().getSDG(jclass);
+    }
+    
+    public SDG getSDG(JavaProject jproject) {
+        return PDGStore.getInstance().getSDG(jproject);
     }
     
     public void setVisible(boolean visible) {
