ae9afe2a56b2aa1e6685d2ecdd03184f64512c73
Katsuhisa Maruyama <maru@cs.ritsumei.ac.jp>
2020-06-16 11:27:00 +0900

    refactoring

--- org.jtool.eclipse/src/main/java/org/jtool/eclipse/batch/ModelBuilderBatch.java
+++ org.jtool.eclipse/src/main/java/org/jtool/eclipse/batch/ModelBuilderBatch.java
@@ -23,7 +23,6 @@ import java.util.ArrayList;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.Set;
-import java.util.HashSet;
 import java.util.Arrays;
 import java.io.File;
 import java.io.FileReader;
